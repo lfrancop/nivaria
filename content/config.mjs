baa471650a2892a299b68ad1404e295488832671
lfrancop <leofp7@gmail.com>
2024-05-01 09:23:25 -0300

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -7,7 +7,7 @@ const config = {
   title: "Nivária",
   showSidebar: true,
   showToc: true,
-  navLinks: [{ href: "/Público/Aventuras", name: "Aventuras" },{ href: "/Público/Conhecimento", name: "Conhecimento" },{ href: "/Público/História", name: "História" }],
+  navLinks: [{ href: "Público/Aventuras/Fuga%20de%20Geisterberg", name: "Fuga de Geisterberg" },{ href: "/Público/Aventuras/Nação%20da%20Luz", name: "Nação da Luz" },{ href: "/Público/Conhecimento/Calendário", name: "Calendário" },{ href: "/Público/Conhecimento/Cultura%20e%20Costumes", name: "Calendário" },{ href: "/Público/Conhecimento/Divisões%20Militares", name: "Calendário" },{ href: "/Público/História/História%20Curta", name: "Calendário" },{ href: "/Público/História/História%20Densa", name: "Calendário" },{ href: "/Público/História/Linha%20do%20Tempo", name: "Calendário" }],
 };
 
 export default config;
