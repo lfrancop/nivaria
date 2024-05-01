fd68a98447f0dfdd055e697744eb3d56b7168a71
lfrancop <leofp7@gmail.com>
2024-05-01 09:25:52 -0300

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -7,7 +7,7 @@ const config = {
   title: "Nivária",
   showSidebar: true,
   showToc: true,
-  navLinks: [{ href: "Público/Aventuras/Fuga%20de%20Geisterberg", name: "Fuga de Geisterberg" },{ href: "/Público/Aventuras/Nação%20da%20Luz", name: "Nação da Luz" },{ href: "/Público/Conhecimento/Calendário", name: "Calendário" },{ href: "/Público/Conhecimento/Cultura%20e%20Costumes", name: "Calendário" },{ href: "/Público/Conhecimento/Divisões%20Militares", name: "Calendário" },{ href: "/Público/História/História%20Curta", name: "Calendário" },{ href: "/Público/História/História%20Densa", name: "Calendário" },{ href: "/Público/História/Linha%20do%20Tempo", name: "Calendário" }],
+  navLinks: [{ href: "Público/Aventuras/Fuga%20de%20Geisterberg", name: "Fuga de Geisterberg" },{ href: "/Público/Aventuras/Nação%20da%20Luz", name: "Nação da Luz" },{ href: "/Público/Conhecimento/Calendário", name: "Calendário" },{ href: "/Público/Conhecimento/Cultura%20e%20Costumes", name: "Cultura & Costumes" },{ href: "/Público/Conhecimento/Divisões%20Militares", name: "Divisões Militares" },{ href: "/Público/História/História%20Curta", name: "História Curta" },{ href: "/Público/História/História%20Densa", name: "História Densa" },{ href: "/Público/História/Linha%20do%20Tempo", name: "Linha do Tempo" }],
 };
 
 export default config;
